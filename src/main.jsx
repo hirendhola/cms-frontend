461401e417aaa081d0cb6a4fcaf57c3c0f8f444f
hirendhola <hirendholap@gmail.com>
2024-09-16 12:14:22 +0530

    vercel SpeedInsights

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')).render(
   <Router>
